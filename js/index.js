a40b9f0f87eb4c2809daa8143e53234b1caa6795
itscharlesm <itscharlesm@gmail.com>
2026-06-06 12:12:57 +0800

    message changes

--- js/index.js
+++ js/index.js
@@ -56,7 +56,7 @@ const PUZZLES = [
         card: {
             emoji: '🌙',
             num: '5 of 7',
-            title: 'Five million ⭐ performance!',
+            title: 'Five ⭐ beauty!',
             msg: 'Five puzzles solved! Your eyes, forehead, cheeks, lips, nose makes you the most beautiful person I know. Bonus is you\'re also sexy and hot respectfully. I love you mommyyy!',
         }
     },
@@ -80,7 +80,7 @@ const PUZZLES = [
         final: true,
         card: {
             title: 'You complete my life 💖',
-            msg: 'Just like these puzzles, you\'re always my missing piece. You complete my life, and that\'s what you mean to me. I don\'t know how many moves you\'ve made, but I hope you will not give up on us, just like how you solved all the puzzles. No matter how many obstacles we may face, no matter how long it takes, no matter how many down days we\'ll have, days when we don\'t understand each other, days when we\'ll be apart, I hope you will keep trying because I will never give up on you. I love you, now and always, my Iah. May we continue to solve life\'s puzzle together, hand in hand, heart to heart. Happy 7th Monthsary! More monthsaries to come! I loveeee youuuu my pretty baby nurse iah ♡',
+            msg: 'Just like these puzzles, you\'re always my missing piece. You complete my life, and that\'s what you mean to me. I don\'t know how many moves you\'ve made, but I hope you will not give up on us, just like how you solved all the puzzles. No matter how many obstacles we may face, no matter how long it takes, no matter how many down days we\'ll have, days when we don\'t understand each other, days when we\'ll be apart, I hope you will keep trying because I will never give up on you. May we continue to solve life\'s puzzle together, hand in hand, heart to heart. Happy 7th Monthsary! More monthsaries to come! I love you, now and always, my pretty Iah ♡',
         }
     },
 ];
